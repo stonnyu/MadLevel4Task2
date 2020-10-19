07a9de5e922ca33efe3e05c15731e8d8a818315c
Tonny <mrtonnysu@gmail.com>
2020-10-19 03:35:19 +0200

    Added navigation to match history page

--- app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.madlevel4task2.R
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        navController = findNavController(R.id.nav_host_fragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -25,10 +30,12 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_match_history -> true
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_match_history -> navController.navigate(
+                R.id.action_matchFragment_to_matchHistoryFragment
+            )
         }
+        return super.onOptionsItemSelected(item)
     }
 
 }
